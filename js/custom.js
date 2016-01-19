b65195d3cf86b9c68a2acc3c68f092dc025d1160
ranjit bhatta <ranjit@parewalabs.com>
2016-01-19 21:24:05 +0545

    Fixed issue with about us section

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
-﻿$(".question").click(function () {
+﻿$(".question").click(function (e) {
+    e.preventDefault();
     if ($(this).hasClass("open")) {
         $(this).siblings(".answer").slideUp("fast");
         $(this).removeClass("open");
@@ -8,3 +9,12 @@
         $(this).siblings(".answer").slideDown("fast");
     }
 });
+
+//$(".about-salsa-header").click(function (e) {
+//    e.preventDefault();
+
+//    $("#popup").html($(this).siblings(".about-salsa-description").html());
+//    $("#popup").show();
+
+//});
+
