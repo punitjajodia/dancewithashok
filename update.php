88f29857f4e28ee5b1534e55c5d25b3558814f2d
ranjit bhatta <ranjit@parewalabs.com>
2016-01-27 11:26:48 +0545

    Updated shell script to hard reset HEAD

--- update.php
+++ update.php
@@ -10,7 +10,8 @@
 	$commands = array(
 		'echo $PWD',
 		'whoami',
-		'git pull',
+		'git reset --hard HEAD',
+		'git pull origin master',
 		'git status',
 		'git submodule sync',
 		'git submodule update',
